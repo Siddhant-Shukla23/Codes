c3300e2095948d79785729d8027d1397564997af
Siddhant-Shukla23 <113886712+Siddhant-Shukla23@users.noreply.github.com>
2022-09-20 02:19:55 +0530

    Time: 220 ms (31.55%), Space: 23.4 MB (35.54%) - LeetHub

---
+++ 15-3sum/15-3sum.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& num) {
+        sort(num.begin(),num.end());
+         vector<vector<int>> res;
+        for(int i=0;i<num.size()-2;i++){
+            if(i==0||(i>0 && num[i]!=num[i-1]) ){
+                int low=i+1;
+                int high=num.size()-1;
+                int sum=0-num[i];
+                while(low<high){
+                    if(num[low]+num[high]==sum){
+                        vector<int> temp;
+                        temp.push_back(num[i]);
+                        temp.push_back(num[low]);
+                        temp.push_back(num[high]);
+                        res.push_back(temp);
+                        
+                        while(low<high && num[low]==num[low+1]) low++;
+                        while(low<high && num[high]==num[high-1]) high--;
+                        
+                        low++,high--;
+                        
+                    }
+                    else if(num[low]+num[high]<sum){
+                       low++;
+                        
+                }
+                    else  high--;
+                        
+            }
+            }
+        }
+        return res;
+    }
+};
