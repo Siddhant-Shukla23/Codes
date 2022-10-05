587d4b4ea7a90df44c36443e2662f6f4c3739510
Siddhant-Shukla23 <113886712+Siddhant-Shukla23@users.noreply.github.com>
2022-10-05 17:49:58 +0530

    Time: 8 ms (48.95%), Space: 9.5 MB (78.35%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,26 +1,34 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int row=matrix.size();
-        int col=matrix[0].size();
+        int rows=matrix.size();
+        int cols=matrix[0].size();
         
-        int s=0;
-        int e=row*col-1;
-        int mid=s+(e-s)/2;
+//         int s=0;
+//         int e=row*col-1;
+//         int mid=s+(e-s)/2;
         
-        while(s<=e){
-            int element=matrix[mid/col][mid%col];
+//         while(s<=e){
+//             int element=matrix[mid/col][mid%col];
             
-            if(element==target){
-                return 1;
-            }
-            if(element>target){
-                e=mid-1;
-            }
-            if(element<target){
-                s=mid+1;
-            }
-            mid=s+(e-s)/2;
+//             if(element==target){
+//                 return 1;
+//             }
+//             if(element>target){
+//                 e=mid-1;
+//             }
+//             if(element<target){
+//                 s=mid+1;
+//             }
+//             mid=s+(e-s)/2;
+//         }
+//         return 0;
+        int row=0;
+        int col=cols-1;
+        while(row<rows && col>-1){
+            if(matrix[row][col]==target) return 1;
+            else if(matrix[row][col]<target) row++;
+            else col--;
         }
         return 0;
     }
