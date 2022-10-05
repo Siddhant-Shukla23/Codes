2469c66c3781bfef93752f3d3cf651e3a1b9ea8a
Siddhant-Shukla23 <113886712+Siddhant-Shukla23@users.noreply.github.com>
2022-10-05 17:59:27 +0530

    Time: 5 ms (71.99%), Space: 9.6 MB (5.11%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -4,32 +4,32 @@ public:
         int rows=matrix.size();
         int cols=matrix[0].size();
         
-//         int s=0;
-//         int e=row*col-1;
-//         int mid=s+(e-s)/2;
+        int s=0;
+        int e=rows*cols-1;
+        int mid=s+(e-s)/2;
         
-//         while(s<=e){
-//             int element=matrix[mid/col][mid%col];
+        while(s<=e){
+            int element=matrix[mid/cols][mid%cols];
             
-//             if(element==target){
-//                 return 1;
-//             }
-//             if(element>target){
-//                 e=mid-1;
-//             }
-//             if(element<target){
-//                 s=mid+1;
-//             }
-//             mid=s+(e-s)/2;
-//         }
-//         return 0;
-        int row=0;
-        int col=cols-1;
-        while(row<rows && col>-1){
-            if(matrix[row][col]==target) return 1;
-            else if(matrix[row][col]<target) row++;
-            else col--;
+            if(element==target){
+                return 1;
+            }
+            if(element>target){
+                e=mid-1;
+            }
+            if(element<target){
+                s=mid+1;
+            }
+            mid=s+(e-s)/2;
         }
         return 0;
+        // int row=0;
+        // int col=cols-1;
+        // while(row<rows && col>-1){
+        //     if(matrix[row][col]==target) return 1;
+        //     else if(matrix[row][col]<target) row++;
+        //     else col--;
+        // }
+        // return 0;
     }
 };
