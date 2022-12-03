63009bc1cc31ad07668add18d0efc27206506611
Siddhant Shukla <113886712+Siddhant-Shukla23@users.noreply.github.com>
2022-12-03 14:36:41 +0530

    Added solution - LeetHub

--- Construct Tree from Inorder & Preorder - GFG/construct-tree-from-inorder-preorder.cpp
+++ Construct Tree from Inorder & Preorder - GFG/construct-tree-from-inorder-preorder.cpp
@@ -41,33 +41,31 @@ struct Node
 */
 class Solution{
     public:
-    int findPosition(int in[], int element, int n){
+    void createMapping(int in[],map<int,int> &nodeToIndex, int n){
         for(int i=0; i<n;i++){
-            if(in[i]==element){
-                return i;
-            }
-          
+           nodeToIndex[in[i]] = i;
         }
-          return -1;
     }
-    Node* solve(int in[],int pre[], int &index, int inOrderStart,int inOrderEnd, int n){
+    Node* solve(int in[],int pre[], int &index, int inOrderStart,int inOrderEnd, int n, map<int,int> &nodeToIndex){
         if(index>=n || inOrderStart > inOrderEnd){
             return NULL;
         }
         
         int element = pre[index++];
         Node* root = new Node(element);
-        int position = findPosition(in, element, n);
+        int position = nodeToIndex[element];
         
-        root->left = solve(in, pre, index, inOrderStart, position-1, n);
-        root->right = solve(in, pre, index, position+1,inOrderEnd , n);
+        root->left = solve(in, pre, index, inOrderStart, position-1, n,nodeToIndex);
+        root->right = solve(in, pre, index, position+1,inOrderEnd , n, nodeToIndex);
         
         return root;
     }
     Node* buildTree(int in[],int pre[], int n)
     {
         int preOrderIndex=0;
-        Node* ans = solve(in, pre, preOrderIndex, 0, n-1, n);
+        map<int,int> nodeToIndex;
+        createMapping(in, nodeToIndex, n);
+        Node* ans = solve(in, pre, preOrderIndex, 0, n-1, n, nodeToIndex);
         return ans;
     }
 };
